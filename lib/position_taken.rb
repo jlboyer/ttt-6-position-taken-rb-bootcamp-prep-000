f9ff91146856ab046df191d75c060760254e6884
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-05-15 22:23:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,9 @@
 # code your #position_taken? method here!
 
-def position_taken?(input)
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  else
+    true
+  end
+end
